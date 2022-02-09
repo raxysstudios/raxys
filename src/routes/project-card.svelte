8a70210840a86fb0751dbe0445e8d591f9385ff2
alkaitagi <alkaitagi@outlook.com>
2022-02-09 19:01:24 +0300

    Improve rounding

--- src/routes/project-card.svelte
+++ src/routes/project-card.svelte
@@ -2,9 +2,9 @@
 	export let iconUrl = 'src/raxys-color.png';
 </script>
 
-<div class="bg-white shadow-md hover:shadow-lg rounded-md flex flex-col p-4 gap-4 w-72">
+<div class="bg-white shadow-md hover:shadow-lg rounded-sm flex flex-col p-4 gap-4 w-72">
 	<div class="flex flex-row items-center gap-4">
-		<img class="w-12 rounded-lg" src={iconUrl} alt="icon" />
+		<img class="w-12 rounded-md" src={iconUrl} alt="icon" />
 		<h3 class="text-2xl font-bold"><slot name="title" /></h3>
 	</div>
 	<p class="flex grow"><slot name="description" /></p>
