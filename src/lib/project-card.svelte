159b70135166459e6d66ac7cfbd6e86adacaf21c
alkaitagi <alkaitagi@outlook.com>
2022-02-09 23:38:31 +0300

    Fix paths

--- src/lib/project-card.svelte
+++ src/lib/project-card.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	export let iconUrl = 'raxys-color.png';
 </script>
 
 <div class="bg-white shadow-md hover:shadow-lg rounded-sm flex flex-col p-4 gap-4 w-72">
 	<div class="flex flex-row items-center gap-4">
-		<img class="w-12 rounded-md" src={iconUrl} alt="icon" />
+		<img class="w-12 rounded-md" src={base + '/' + iconUrl} alt="icon" />
 		<h3 class="text-2xl font-bold flex-1"><slot name="title" /></h3>
 	</div>
 	<p class="flex grow"><slot name="description" /></p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const production = process.env.NODE_ENV !== 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
+		paths: {
+			base: production ? '/raxys' : undefined
+		},
 		adapter: adapter()
 	}
 };
