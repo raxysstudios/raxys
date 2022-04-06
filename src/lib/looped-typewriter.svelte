29c667efb313691ce5e4405a1bd8d2c874fee958
alkaitagi <alkaitagi@outlook.com>
2022-04-07 02:21:26 +0300

    Refactor shuffle function

--- src/lib/looped-typewriter.svelte
+++ src/lib/looped-typewriter.svelte
@@ -1,22 +1,8 @@
 <script>
 	import { onMount } from 'svelte';
+	import { shuffle } from './utils/shuffle';
 
 	export let strings = [];
-
-	function shuffle(array) {
-		let currentIndex = array.length;
-		let randomIndex = 0;
-		while (currentIndex != 0) {
-			// Pick a remaining element...
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex--;
-
-			// And swap it with the current element.
-			[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-		}
-
-		return array;
-	}
 
 	let quote;
 	onMount(
