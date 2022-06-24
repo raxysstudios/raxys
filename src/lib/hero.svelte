5a5526d4607d6fc821aa8844d01e1d6f5e2c9df9
alkaitagi <alkaitagi@outlook.com>
2022-06-25 00:42:13 +0300

    Update icons

--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -4,26 +4,15 @@
 </script>
 
 <div data-tilt id="parent" class="relative group cursor-pointer select-none">
-	<img class="drop-shadow-lg thm-img group-active:opacity-40" src={raxys} alt="raxys" />
+	<img class="drop-shadow-lg thm-img" src={raxys} alt="raxys" />
 	<div
-		id="first"
-		class="absolute top-0 w-64 h-64 opacity-0 scale-50 thm-img group-hover:opacity-40"
+		id="back"
+		class="absolute top-0 w-64 h-64 opacity-0 scale-50 thm-img group-hover:opacity-40 group-active:opacity-100"
 	>
 		<img class="left-[33.5%] top-[33.5%]" src={raxys} alt="raxys" />
 		<img class="-left-[33.5%] -top-[33.5%]" src={raxys} alt="raxys" />
 		<img class="left-[33.5%] -top-[33.5%]" src={raxys} alt="raxys" />
 		<img class="-left-[33.5%] top-[33.5%]" src={raxys} alt="raxys" />
-		<img class="-left-[67%] top-0" src={raxys} alt="raxys" />
-		<img class="left-[67%] top-0" src={raxys} alt="raxys" />
-	</div>
-	<div
-		id="second"
-		class="absolute top-0 w-64 h-64 opacity-0 scale-50 thm-img group-active:opacity-40"
-	>
-		<img class="left-[99.5%] top-[33.5%]" src={raxys} alt="raxys" />
-		<img class="-left-[99.5%] -top-[33.5%]" src={raxys} alt="raxys" />
-		<img class="left-[99.5%] -top-[33.5%]" src={raxys} alt="raxys" />
-		<img class="-left-[99.5%] top-[33.5%]" src={raxys} alt="raxys" />
 	</div>
 </div>
 
@@ -44,10 +33,7 @@
 	#parent > div > img:nth-child(odd) {
 		transform: translateX(-32px);
 	}
-	#parent:hover > #first > img {
-		transform: translate(0);
-	}
-	#parent:active > #second > img {
+	#parent:hover > #back > img {
 		transform: translate(0);
 	}
 	#parent:hover > img {
