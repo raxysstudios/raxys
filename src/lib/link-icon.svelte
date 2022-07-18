4a565d5dcb457bf10a32043321dd5df8ca946336
alkaitagi <alkaitagi@outlook.com>
2022-07-18 22:52:48 +0300

    Reinstall iconify

--- src/lib/link-icon.svelte
+++ src/lib/link-icon.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	export let url = '';
 	export let icon = '';
 </script>
 
 <a href={url} class="p-1 hover:scale-125">
-	<span class="iconify" data-icon={icon} data-inline="false" />
+	<Icon {icon} />
 </a>
