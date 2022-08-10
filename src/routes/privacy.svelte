7d93971b1f55a1197521a82fc3ad866324fdacf0
alkaitagi <alkaitagi@outlook.com>
2022-08-10 06:09:16 +0300

    Add privacy policy page

---
+++ src/routes/privacy.svelte
@@ -0,0 +1,16 @@
+<div class="p-4 flex flex-col gap-2">
+	<h2>Raxys Studios Privacy Policy</h2>
+
+	<p>
+		We do not use nor store any of your personal data. Hopefully you'll find our apps to be helpful!
+	</p>
+
+	<p>
+		Мы не используем и не храним ваши личные данные. Надеемся, наши приложения будут вам полезны!
+	</p>
+
+	<p>
+		Kişisel verilerinizin hiçbirini kullanmıyoruz veya saklamıyoruz. Umarız uygulamalarımızı faydalı
+		bulursunuz!
+	</p>
+</div>
